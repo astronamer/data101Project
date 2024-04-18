a66781c367fd0e9cb26f8e388b7541323cb4aa3d
astronamer <astronamer20@gmail.com>
2024-04-18 16:16:22 -0400

    added naive bayes predictions
    still needs tuning

--- Data101Projectpt2.R
+++ Data101Projectpt2.R
@@ -14,22 +14,53 @@ cleanData<-function(data){#function to clean dataframe
   data$CoapplicantIncome<-as.integer(data$CoapplicantIncome)
   data$Credit_History<-factor(data$Credit_History)
   data$Property_Area<-factor(data$Property_Area)
+  data$totincome<-data$ApplicantIncome+data$CoapplicantIncome
+  data$loanOverIncome<-(data$LoanAmount/data$totincome)
   data
 }
 model<-function(frame){#creates the tree for the training data
   library(rpart)
-  tree<-rpart(Loan_Status ~ Gender + Married + Dependents + Education + Self_Employed + ApplicantIncome + CoapplicantIncome + LoanAmount + Loan_Amount_Term + Credit_History + Property_Area, data=frame, control=rpart.control(cp=.005), method="class")
+  tree<-rpart(Loan_Status ~ totincome + Gender + Married + Dependents + Education + Self_Employed + ApplicantIncome + CoapplicantIncome + LoanAmount + Loan_Amount_Term + Credit_History + Property_Area, data=frame, control=rpart.control(cp=.005), method="class")
   tree
 }
 graphTree<-function(tree){#graphs the tree
   library(rpart.plot)
   rpart.plot(tree,roundint=FALSE, box.palette ="RdYlGn")
 }
+NaiveBayes<-function(frame){#creates a naive bayes model out of the dataframe
+  library(e1071)
+  naive_bayes_model <- naiveBayes(Loan_Status ~ loanOverIncome + totincome + Gender + Married + Dependents + Education + Self_Employed + ApplicantIncome + CoapplicantIncome + LoanAmount + Loan_Amount_Term + Credit_History + Property_Area, data=frame)
+  naive_bayes_model
+
+}
+NaivePredict<-function(model, frame){#makes a prediction based off of the naive bayes model model
+  frame$predictions <- predict(model, newdata = frame)
+  frame
+}
+#load library
 library(rpart)
+
+#make the dataframes
 train<-readtrain()
+test<-readtest()
+
+#make a decision tree model
 tree<-model(train)
 graphTree(tree)
-test<-readtest()
+
+#use the decision tree to predict loan status
 pred_Loan_Status<-rpart.predict(tree, train, type='class')
 test$pred_Loan_Status<-rpart.predict(tree, test, type='class')
+
+#print a confusion matrix from predictions
 table(pred_Loan_Status, train$Loan_Status)
+
+#generate a naive bayes model
+model<-NaiveBayes(train)
+
+#predict the loan status using a naive bayes model
+train<-NaivePredict(model, train)
+test<-NaivePredict(model, test)
+
+#print a confusion matrix based on the predictions of the naive bayes model
+table(train$predictions, train$Loan_Status)
